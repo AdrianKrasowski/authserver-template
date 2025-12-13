79e46a9cf0a2005a9e441ee65b3c283cff5db6a4
Adrian Krasowski <Adr.Krasowski@gmail.com>
2025-12-13 20:28:18 +0100

    Initial DbContext Setup

---
+++ AuthServer.Infrastructure/AuthDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Infrastructure
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid> 
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.UseOpenIddict();
+
+            builder.HasDefaultSchema("auth");
+        }
+    }
+}
